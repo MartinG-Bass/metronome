a62bcfaddd5f60715eb1db8b12871bb455ba149d
MartinG-Bass <6mgbreto@gmail.com>
2026-03-11 12:33:02 +0100

    Cleaned a bit the code. Added a stop to the sound

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
-const playButton = document.getElementById("playButton");
-playButton.addEventListener("click", () =>{
-    // Crear el contexto de audio
-    let audioContext = new AudioContext();
-    var buffer = audioContext.createBuffer(1, 1, 22050);
-    var node = audioContext.createBufferSource();
-    node.buffer = buffer;
-    node.start(0);
+let audioContext = new AudioContext();
+let tempo = 60.0;
+/* Not needed for now, maybe in the future
 
-    // Crear un oscilador con onda sinusoidal
+var buffer = audioContext.createBuffer(1, 1, 22050);
+var node = audioContext.createBufferSource();
+node.buffer = buffer;
+node.start(0);*/
+
+
+const playButton = document.getElementById("playButton");
+//Funtion to Start the metronome
+playButton.addEventListener("click", () =>{
+
+    //Sine Oscilator
     var osc = audioContext.createOscillator();
     osc.connect( audioContext.destination );
     osc.frequency.value = 880.0;
     osc.start(audioContext.currentTime);
-    console.log("DEbug");
+    const noteLength = 0.05;
+    osc.stop(audioContext.currentTime+noteLength);
+
 
 }); 
 
 const submitTempoBtn = document.getElementById("submitTempo");
 submitTempoBtn.addEventListener("click", () => {
     const inputTempo = document.getElementById("inputTempo");
-    let tempo = inputTempo.value;
-    console.log(tempo);
+    tempo = inputTempo.value;
     inputTempo.value = "";
 });
