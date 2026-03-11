bb4814e59ad8e7b24fb98231d1837fcae06f2c5d
MartinG-Bass <6mgbreto@gmail.com>
2026-03-11 13:23:20 +0100

    Created some functions to have a stable pulse (need to work in the logic of the condition in the loop)

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let audioContext = new AudioContext();
-let tempo = 60.0;
+let tempo = 120.0;
+const noteLength = 0.05;
+const lookAheadTime = 0.1;
+let playing = false;
+let nextBeatTime = 0;
 /* Not needed for now, maybe in the future
 
 var buffer = audioContext.createBuffer(1, 1, 22050);
@@ -10,18 +14,36 @@ node.start(0);*/
 
 const playButton = document.getElementById("playButton");
 //Funtion to Start the metronome
-playButton.addEventListener("click", () =>{
-
-    //Sine Oscilator
-    var osc = audioContext.createOscillator();
-    osc.connect( audioContext.destination );
-    osc.frequency.value = 880.0;
-    osc.start(audioContext.currentTime);
-    const noteLength = 0.05;
-    osc.stop(audioContext.currentTime+noteLength);
+playButton.addEventListener("click", () => {
+    //On/Off
+    playing = !playing;
+    
+    //On
+    if(playing){
+        nextBeatTime = audioContext.currentTime;
+        let i=0;
+        while (i<1000){
+            scheduleBeat();
+            setNextBeat();
+            i++;
+        }
+    }
 
 
 }); 
+
+function scheduleBeat(){
+    
+    const osc = audioContext.createOscillator();
+    osc.connect( audioContext.destination );
+    osc.frequency.value = 880.0;
+    osc.start(nextBeatTime);
+    osc.stop(nextBeatTime+noteLength);
+}
+
+function setNextBeat(){
+    nextBeatTime += 60/tempo;
+}
 
 const submitTempoBtn = document.getElementById("submitTempo");
 submitTempoBtn.addEventListener("click", () => {
